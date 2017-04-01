c39872e374c80b304c2f0820070270d165ad0fe2
ambradford <ashleybradford951@gmail.com>
2017-04-01 05:48:44 -0400

    css stylings on footer, header, and body
    added color thoughout. added pics to 3rd page thumbnails.

--- js/main.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function () {
- 	$(".navbar-nav li a").click(function (event) {
-    
-    	var toggle = $(".navbar-toggle").is(":visible");
-    	if (toggle) {
-     	$(".navbar-collapse").collapse('hide');
-    	}
- 	});
- });	
