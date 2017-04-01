1f6f3792e8cd7fdde9fd16a8d72d67d4c04eb5ef
ambradford <ashleybradford951@gmail.com>
2017-03-31 23:05:49 -0400

    Possiblities section added
    possiblities page added. carousel working correctly with proper
    expanding thumbnails on 2nd page.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ $(document).ready(function () {
+ 	$(".navbar-nav li a").click(function (event) {
+    
+    	var toggle = $(".navbar-toggle").is(":visible");
+    	if (toggle) {
+     	$(".navbar-collapse").collapse('hide');
+    	}
+ 	});
+ });	
